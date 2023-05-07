0b793b4980fcfbdf9fcf79239dd62b15e63b265b
Miguel Patón Gutiérrez <miguelpaton97@gmail.com>
2023-05-07 05:03:09 +0200

    Initial Commit

---
+++ PorraF1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PorraF1
+//
+//  Created by Miguel Patón Gutiérrez on 7/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PorraF1/PorraF1App.swift
@@ -0,0 +1,17 @@
+//
+//  PorraF1App.swift
+//  PorraF1
+//
+//  Created by Miguel Patón Gutiérrez on 7/5/23.
+//
+
+import SwiftUI
+
+@main
+struct PorraF1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
